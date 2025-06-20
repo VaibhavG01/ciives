5c3151697881ce972c10195ffddb5e047e9717e9
Vaibhav <softbyvaibhav01@gmail.com>
2025-06-20 11:22:28 +0530

    add gsap and dr layout

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,17 +1,18 @@
- gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-        const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll("section");
 
-        sections.forEach(section => {
-            gsap.from(section, {
-                scrollTrigger: {
-                    trigger: section,
-                    start: "top 80%",
-                    toggleActions: "play none none none"
-                },
-                opacity: 0,
-                y: 50,
-                duration: 1,
-                ease: "power3.out"
-            });
-        });
+sections.forEach(section => {
+    gsap.from(section, {
+        scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            toggleActions: "play none none none"
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        ease: "power3.out"
+    });
+});
+
